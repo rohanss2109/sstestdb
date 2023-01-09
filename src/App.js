112e63eed7ba8693fd5ed8973f14edc5f300d721
rohanverma2109 <rohanverma2109@gmail.com>
2023-01-09 18:32:48 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,35 @@ const { ipcRenderer } =window.require('electron');
 function App() {
   const [Timer, setTimer] = useState(false);
   const [Sec, setSec] = useState(0);
+  // const [json, setjson] = useState();
+  // const [mysql, setmysql] = useState();
 
+  
+
+  function download(json){
+  const columns = Object.keys(json[0]);
+  const replacer = (key, value) => (value === null ? '' : value);
+  const csv = json.map(row => columns.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+  csv.unshift(columns.join(','));
+  const csvString = csv.join('\r\n');
+
+  const a = document.createElement('a');
+  a.href = URL.createObjectURL(new Blob([csvString], {type: 'text/csv'}));
+  a.download = 'datafromlocal.csv';
+  a.click();
+
+  }
   useEffect(()=>{
     ipcRenderer.on('log',function log(e,data){
+      // download(data);
       console.log(data);
+    })
+    ipcRenderer.on('export',function log(e,data){
+      download(data);
     })
     return()=>{
       ipcRenderer.removeAllListeners("log");
+      ipcRenderer.removeAllListeners("export");
     }
   })
   useEffect(()=>{
@@ -27,7 +49,7 @@ function App() {
         }
       },1000)
     }else{
-      ipcRenderer.send('fetch',{})
+      // ipcRenderer.send('fetch',{})
     }
     return()=>{
       clearInterval(interval);
@@ -37,6 +59,7 @@ function App() {
     <div className="App">
       <h1>{Sec}</h1>
       <button className='btn' onClick={()=>{setTimer(!Timer)}} >{Timer?'Stop':"Start"}</button>
+      <button className='btn' onClick={()=>{ipcRenderer.send('fetch',{})}}>download</button>
     </div>
   );
 }
