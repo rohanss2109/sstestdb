e590a0720cb9e9566e7137cfbe0d6d0943349bf6
rohan <rohan@snakescript.com>
2023-01-12 13:13:37 +0530

    ui and functionality changes

--- public/electron.js
+++ public/electron.js
@@ -1,226 +1,157 @@
-const { app, BrowserWindow, ipcMain} = require('electron')
-const storage = require('electron-json-storage');
+const { app, BrowserWindow, ipcMain, shell, dialog } = require('electron')
 const path = require('path')
 const isDev = require('electron-is-dev')
-const sqlite3 = require('sqlite3').verbose();
 const Excel = require('exceljs');
+const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 const workbook = new Excel.Workbook();
 const worksheet = workbook.addWorksheet('My Sheet');
-// const mysql = require('mysql');
-const appDataPath = app.getPath('appData');
-console.log(appDataPath)
-let mainWindow = null;
-let digit=13
-function setdigit(data){
-    digit=data;
-}
-function newEntry(){
-    let num=null;
-    if(digit===13){
-        num =Math.floor(1000000000000 + Math.random() * 9000000000000)
-    }else{
-        num =Math.floor(100000000000 + Math.random() * 900000000000)
-    }
-    let date= Date.now();
-    // let date= d.toString();
-    return {number:num,date:date};
-}
-function insert(number,date){
-    db.serialize(async () => {
-        // let qry= "DELETE FROM projectbilling"
-        if(number&&date){
-            let qry=`INSERT INTO random ( number , timestamp)VALUES(${number},'${date}');`
-            db.run(qry,function(err){
-            if (err) {
-                    console.error(err.message);
-                    }
-                    console.log("saved Locally");
-                    mainWindow.webContents.send("log",'saved')
-                });
-        }
-        });
-    }
+const appname = app.getName();
+let mydata = app.getPath('appData');
+let appDataPath = false
 
-
-function create(){
-    let qry=`CREATE TABLE IF NOT EXISTS random (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            number INTEGER NOT NULL UNIQUE,
-            timestamp TEXT NOT NULL
-        );`;
-        db.serialize(() => {
-            db.run(qry,function(err){
-            if (err) {
-                    console.error(err.message);
-                    }
-                    console.log("DB TABLE SETUP DONE");
-                });
-        });
-}
-            
-function select(){
-    let qry=`SELECT * FROM random;`;
-    // db.serialize(() => {
-    db.all(qry,[],(err,rows) => {
-        // let qry= "DELETE FROM projectbilling"
-        if (err) {
-            console.error(err);
-            }
-            if(rows){
-                mainWindow.webContents.send("export",rows)
-            }
-            });
-        // });
-
-}
-
-function insertexcel(number,date){
-    // workbook.xlsx.readFile('example.xlsx')
-    // .then(function() {
-    //     // Get the first worksheet
-    //     const worksheet = workbook.getWorksheet(1);
-
-    //     // Add a new row
-    //     worksheet.addRow(['New Person', 40, 'Other']);
-
-    //     // Write the workbook to a file
-    //     workbook.xlsx.writeFile('example.xlsx')
-    //         .then(function() {
-    //             console.log('File is written.');
-    //         });
-    // });
-   
-
-// Add some data to the worksheet
-worksheet.addRow([number, date]);
-let d= new Date(Date.now());
-let file=d.toDateString()
-let name='data'+file+'.xlsx'
-workbook.xlsx.writeFile(path.join(appDataPath, 'sstestdb', name))
-    .then(function() {
-        console.log('File is written.');
-    });
-}
-
-            
-            
-// function deleterow (id){
-//     db.serialize(() => {
-//         if(id){
-//             let qry= `DELETE FROM random WHERE id=${id}`;
-//             db.run(qry,function(err){
-//             if (err) {
-//                     console.error(err.message);
-//                     mainWindow.webContents.send("log",{err})
-//                 }
-//                 mainWindow.webContents.send("log","cong")
-//                     console.log("deleted Locally");
-//                 });
-//         }
-//         });
-// }
-            
-let db = new sqlite3.Database(path.join(appDataPath, 'sstestdb', 'database.db'), (err) => {
-    if (err) {
-        console.error(err.message);
-    }else{
-        console.log('Connected to the sqlite database.');
-        create();
-    }
-    });
-              
-function createMainWindow () {
+function createMainWindow() {
     mainWindow = new BrowserWindow({
         width: 400,
         height: 400,
-        resizable:false,
+        resizable: false,
         webPreferences: {
-            nodeIntegration:true,
+            nodeIntegration: true,
             preload: path.join(__dirname, 'preload.js')
         }
     })
     mainWindow.loadURL(
-          isDev
+        isDev
             ? 'http://localhost:3000'
             : `file://${path.join(__dirname, 'index.html')}`
-        )
+    )
 }
-// try {
-//     var connection = mysql.createConnection({
-//         host     : 'localhost',
-//         user     : 'root',
-//         password : '',
-//         database : 'temp'
-//       });
-//       connection.connect();
-// } catch (error) {
-//     mainWindow.webcontents.send('log',{error});
-// }
+
 app.on('ready', () => {
- createMainWindow();
- try {
-    var data = storage.getSync('Data');
-       if(!data.length){
-        storage.set('Data',[], function(error) {
-                            if (error) throw error;
-                          });
-       }  
-} catch (error) {
-    mainWindow.webContents.send('log',{error});
-}
+    createMainWindow();
+    try {
+        var data = storage.getSync('Data');
+        if (!data.length) {
+            storage.set('Data', [], function (error) {
+                if (error) throw error;
+            });
+        }
+    } catch (error) {
+        mainWindow.webContents.send('log', { error });
+    }
 
 })
 
-ipcMain.on('save',()=>{
-    let Entry=newEntry()
-    insert(Entry.number,Entry.date)
-    insertexcel(Entry.number,Entry.date)
-// try {
-//     let data = storage.getSync('Data');
-//     data.push(Entry)
-//     storage.set('Data',data, function(error) {
-//         if (error) throw error;
-//       });
-// } catch (error) {
-//     mainWindow.webContents.send('log',{error});
-// }
-// try {
-//     let qry = `INSERT INTO random (number, date) VALUES ('${Entry.number}', '${Entry.date}')`;
+let db = new sqlite3.Database(path.join(mydata, appname, 'database.db'), (err) => {
+    if (err) {
+        console.error(err.message);
 
-//         connection.query(qry, function (error, results, fields) {
-//             if (error) console.log(error.code);
-//             else {
-//                 // console.log(results[0]);
-//                 // $('#resultDiv').text(results[0].emp_name); //emp_name is column name in your database
-//             }
-//         });
-// } catch (error) {
-//     mainWindow.webContents.send('log',{error});
-// }
-
+    } else {
+        console.log('Connected to the sqlite database.');
+        let qry = `CREATE TABLE IF NOT EXISTS pathdata (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            path TEXT NOT NULL
+        );`;
+        db.serialize(() => {
+            db.run(qry, function (err) {
+                if (err) {
+                    console.error(err.message);
+                    mainWindow.webContents.send("log", err);
+                }
+                console.log("DB TABLE SETUP DONE");
+                mainWindow.webContents.send("log", 'rows')
+            });
+        });
+    }
 });
-ipcMain.on('digit',(e,data)=>{
+
+
+
+let mainWindow = null;
+let digit = 13
+function setdigit(data) {
+    digit = data;
+}
+function newEntry() {
+    let num = null;
+    if (digit === 13) {
+        num = Math.floor(1000000000000 + Math.random() * 9000000000000)
+    } else {
+        num = Math.floor(100000000000 + Math.random() * 900000000000)
+    }
+    let date = Date.now();
+    // let date= d.toString();
+    return { number: num, date: date };
+}
+async function getpathfromuser(){
+    const result = await dialog.showOpenDialog(mainWindow, {
+        properties: ['openDirectory']
+    })
+    if (result.filePaths.length > 0) {
+        appDataPath = result.filePaths[0];
+        mainWindow.webContents.send('path', appDataPath);
+        db.serialize(async () => {
+            if (appDataPath) {
+                let qry = `INSERT INTO pathdata ( path )VALUES('${appDataPath}');`
+                await db.run(qry, function (err) {
+                    if (err) {
+                        console.error(err.message);
+                    }
+                    console.log("saved Locally");
+                    mainWindow.webContents.send("log", 'saved')
+                });
+            }
+        });
+    }
+}
+
+function insertexcel(number, date) {
+    worksheet.addRow([number, date]);
+    let d = new Date(Date.now());
+    let file = d.toDateString()
+    let name = 'data' + file + '.xlsx'
+    workbook.xlsx.writeFile(path.join(appDataPath, name))
+        .then(function () {
+            console.log('File is written.');
+        });
+}
+
+
+
+
+ipcMain.on('save', () => {
+    let Entry = newEntry()
+    insertexcel(Entry.number, Entry.date)
+});
+ipcMain.on('digit', (e, data) => {
     setdigit(data.digit)
 })
-ipcMain.on('getpath',(e,data)=>{
-    mainWindow.webContents.send('path',appDataPath);
-})
-ipcMain.on('fetch',()=>{
-//     let data = storage.getSync('Data');
-//     mainWindow.webContents.send('local',data);
-//     try {
-//         let qry = 'SELECT * FROM random';
+ipcMain.on('checkpath', async (e, data) => {
+    if (!appDataPath) {
+        let qry = `SELECT * FROM pathdata;`;
 
-//         connection.query(qry, function (error, results, fields) {
-//             if (error) console.log(error.code);
-//             else {
-//                 mainWindow.webContents.send('mysql',results);
-//                 // $('#resultDiv').text(results[0].emp_name); //emp_name is column name in your database
-//             }
-//         });
-//     } catch (error) {
-//         mainWindow.webContents.send('log',{error});
-//     }
-    select()
-});
+        await db.all(qry, [], (err, rows) => {
+            if (err) {
+                console.error(err);
+            }
+            if (rows[0]) {
+                appDataPath=rows[0].path
+                mainWindow.webContents.send('path', appDataPath);
+                return;
+            }else{
+                getpathfromuser()
+            }
+        });
+       
+    } else {
+        mainWindow.webContents.send('path', appDataPath);
+    }
+})
+// ipcMain.on('getpath',(e,data)=>{
+//     mainWindow.webContents.send('path',appDataPath);
+// })
+ipcMain.on('open', (e, data) => {
+    let d = new Date(Date.now());
+    let file = 'data' + d.toDateString() + '.xlsx'
+    shell.showItemInFolder(path.join(appDataPath,file))
+})
--- src/App.js
+++ src/App.js
@@ -6,105 +6,80 @@ function App() {
   const [Timer, setTimer] = useState(false);
   const [Sec, setSec] = useState(0);
   const [path, setpath] = useState('');
+  const [digit, setdigit] = useState(false);
 
-  function sel(e){
-    if(!e.target.classList.contains('sel')){
-      ipcRenderer.send('getpath',{});
-      let digit=parseInt(e.target.dataset.digit);
-    ipcRenderer.send('digit',{digit:digit})
-    for (let i = 0; i < e.target.closest('.digits').children.length; i++) {
-      const element = e.target.closest('.digits').children[i];
-      element.classList.remove('sel');
-      element.dataset.sel=false;
-    }
-    e.target.classList.add('sel');
-    e.target.dataset.sel=true
-
-    }else{
-
-      e.target.classList.remove('sel');
-    }
-    
+function sel(e){
+  if(!e.target.classList.contains('sel')){
+    ipcRenderer.send('checkpath',{});
+    let digiti=parseInt(e.target.dataset.digit);
+  ipcRenderer.send('digit',{digit:digiti})
+  for (let i = 0; i < e.target.closest('.digits').children.length; i++) {
+    const element = e.target.closest('.digits').children[i];
+    element.classList.remove('sel');
   }
-  function start(e){
-    e.preventDefault();
-    let sel=false;
-    // let digits=
-    console.log(document.getElementById('digits').children);
-    for (let i = 0; i < document.getElementById('digits').children.length; i++) {
-      const element = document.getElementById('digits').children[i];
-      let temp=element.classList.contains('sel');
-      if(temp===true){
-        sel=true;
-      }
-    }
-    if(!sel){
-      if(Timer){
-        alert('Please select a digit first')
-      }
-    }
-    if(sel){
-      setTimer(true);
-    }else{
-      setTimer(false);
-    }
+  e.target.classList.add('sel');
+  }else{
+    e.target.classList.remove('sel');
+    setdigit(false)
+    setTimer(false);
   }
-
-  function download(json){
-  const columns = Object.keys(json[0]);
-  const replacer = (key, value) => (value === null ? '' : value);
-  const csv = json.map(row => columns.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
-  csv.unshift(columns.join(','));
-  const csvString = csv.join('\r\n');
-
-  const a = document.createElement('a');
-  a.href = URL.createObjectURL(new Blob([csvString], {type: 'text/csv'}));
-  a.download = 'datafromlocal.csv';
-  a.click();
-
-  }
-  useEffect(()=>{
-    ipcRenderer.on('path',function log(e,data){
-    let pathdata= 'Your file is saved at '+data+'/sstestdb/';
-      setpath(pathdata);
-    })
-   
-    return()=>{
-      ipcRenderer.removeAllListeners("path");
-    }
-  })
-  useEffect(()=>{
-    ipcRenderer.on('log',function log(e,data){
-      // download(data);
-      console.log(data);
-    })
-    ipcRenderer.on('export',function log(e,data){
-      download(data);
-    })
-    return()=>{
-      ipcRenderer.removeAllListeners("log");
-      ipcRenderer.removeAllListeners("export");
-    }
-  })
-  useEffect(()=>{
-    let interval=null;
+  
+}
+function start(e){
+  e.preventDefault();
+  if(digit){
     if(Timer){
-      interval=setInterval(()=>{
-        if(Timer){
-          setSec(Sec+1);
-          let res=Sec%2
-          if(res===0){
-            ipcRenderer.send('save',{})
-          }
-        }
-      },1000)
+      setTimer(false);
     }else{
-      // ipcRenderer.send('fetch',{})
+      setTimer(true);
     }
-    return()=>{
-      clearInterval(interval);
-    }
+  }else{
+    alert('Please select a digit first')
+  }
+}
+
+
+useEffect(()=>{
+  ipcRenderer.on('path',function log(e,data){
+  let pathdata= 'Your file is saved at '+data;
+    setpath(pathdata);
+    setdigit(true);
   })
+  
+  return()=>{
+    ipcRenderer.removeAllListeners("path");
+  }
+})
+
+useEffect(()=>{
+  ipcRenderer.on('log',function log(e,data){
+    console.log(data);
+  })
+  return()=>{
+    ipcRenderer.removeAllListeners("log");
+  }
+})
+
+
+useEffect(()=>{
+let interval=null;
+if(Timer){
+  interval=setInterval(()=>{
+    if(Timer){
+      setSec(Sec+1);
+      let res=Sec%2
+      if(res===0){
+        ipcRenderer.send('save',{})
+      }
+    }
+  },1000)
+}else{
+  // ipcRenderer.send('fetch',{})
+}
+return()=>{
+  clearInterval(interval);
+}
+})
   return (
     <div className="App">
       <h1>{Sec}</h1>
@@ -113,7 +88,9 @@ function App() {
         <h4 data-digit='13' className='btn' onClick={sel}>13 digit</h4>
       </div>
       <button className='btn' onClick={start} >{Timer?'Stop':"Start"}</button>
-      {/* <button className='btn' onClick={()=>{ipcRenderer.send('fetch',{})}}>download</button> */}
+      {(path&&digit&&Sec>2)?
+      <button className='btn' onClick={()=>{ipcRenderer.send('open',{})}}>open</button>
+      :<></>}
       <h6 id='path'>{path}</h6>
     </div>
   );
